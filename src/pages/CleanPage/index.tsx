f7c39bac88a36eca0d9a1e7877c2324c07e1c012
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-03-01 18:12:42 -0300

    fix: just adding some text

--- src/pages/CleanPage/index.tsx
+++ src/pages/CleanPage/index.tsx
@@ -1,13 +1,22 @@
 /** Packages */
 import React from 'react';
+import { useAuth } from '../../context/auth';
 
 /** My Components */
 import { Container } from './styles';
 
 const CleanPage: React.FC = () => {
+  const { user, signOut } = useAuth();
+
   return (
     <Container>
-      <span>CleanPage</span>
+      <h1>Main page</h1>
+      <span>Logado como:</span>
+      <span>{user}</span>
+
+      <button type="button" onClick={signOut}>
+        <span>Deslogar</span>
+      </button>
     </Container>
   );
 };
