bc32599e99e0f02482ff4a2eb9846fa1f6e3b4c2
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-03-01 16:15:43 -0300

    style: stylishing login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,36 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { FiArrowRight } from 'react-icons/fi';
 import Logo from '../../assets/logo-lol.svg';
 import Wallpaper from '../../assets/wallpaper-client.jpg';
 
 import Input from '../../components/Input';
+import { useAuth } from '../../context/auth';
 
 import {
   Container,
   UserLogin,
   Title,
   SocialLogin,
-  KeepLogged,
   LogOn,
   Links,
   Client,
   Notify,
   Settings,
   Background,
+  QuestionIcon,
+  Dialog,
 } from './styles';
 
 const Login: React.FC = () => {
+  const { signIn, userDataFromInput } = useAuth();
+
+  const [showDialog, setShowDialog] = useState(false);
+
   return (
     <Container>
       <UserLogin>
+        {showDialog && (
+          <Dialog
+            onMouseEnter={() => setShowDialog(true)}
+            onMouseLeave={() => setShowDialog(false)}
+          >
+            <span>Clone criado por Pedro H. usando ReactJS</span>
+          </Dialog>
+        )}
+
+        <QuestionIcon
+          size={20}
+          onMouseEnter={() => setShowDialog(true)}
+          onMouseLeave={() => setShowDialog(false)}
+          show={showDialog}
+        />
         <img src={Logo} alt="Logo LoL" />
         <Title>Fazer login</Title>
         <Input text="Nome de usuário" />
-        {/* <Input text="senha" /> */}
 
-        <SocialLogin />
-        <KeepLogged />
-        <LogOn />
+        <LogOn
+          type="button"
+          isDisabled={userDataFromInput.length > 1}
+          onClick={
+            () =>
+              userDataFromInput.length > 1 ? signIn(userDataFromInput) : ''
+            // eslint-disable-next-line react/jsx-curly-newline
+          }
+        >
+          <FiArrowRight size={40} />
+        </LogOn>
 
         <Links />
       </UserLogin>
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,4 +1,13 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { FaQuestionCircle } from 'react-icons/fa';
+
+interface LogOnProps {
+  isDisabled?: boolean;
+}
+
+interface DialogProps {
+  show?: boolean;
+}
 
 export const Container = styled.div`
   display: flex;
@@ -12,6 +21,7 @@ export const Container = styled.div`
 `;
 
 export const UserLogin = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -19,27 +29,62 @@ export const UserLogin = styled.div`
 
   min-width: 400px;
 
-  background-color: rgba(255, 255, 255, 0.96);
+  background: #fdfcff;
+  opacity: 0.98;
 
   > img {
     width: 40px;
     margin: 30px 0;
+
+    pointer-events: none;
   }
 `;
 
 export const Title = styled.span`
   font-weight: bold;
-
   font-size: 20px;
 
   margin: 10px 0;
+  pointer-events: none;
 `;
 
 export const SocialLogin = styled.div``;
 
-export const KeepLogged = styled.div``;
+export const LogOn = styled.button<LogOnProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-export const LogOn = styled.div``;
+  width: 70px;
+  height: 70px;
+
+  margin: 30px 0;
+
+  border: 2px solid gray;
+  border-radius: 15px;
+
+  cursor: default;
+  background: #eee;
+
+  opacity: 0.2;
+
+  transition: all 300ms ease;
+
+  ${p =>
+    p.isDisabled &&
+    css`
+      border: 2px solid #d43537;
+      background: #d43537;
+      opacity: 1;
+
+      cursor: pointer;
+
+      svg {
+        color: #fff;
+        transition: all 300ms ease;
+      }
+    `};
+`;
 
 export const Links = styled.div``;
 
@@ -61,5 +106,54 @@ export const Background = styled.div`
   img {
     object-fit: cover;
     width: 100%;
+  }
+`;
+
+export const QuestionIcon = styled(FaQuestionCircle)<DialogProps>`
+  position: absolute;
+  top: 30px;
+  right: 20px;
+
+  color: #e1e1e3;
+`;
+
+export const Dialog = styled.div`
+  position: absolute;
+  top: 10px;
+  right: -244px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+
+  background: #fff;
+
+  width: 250px;
+  height: 100px;
+
+  padding: 15px;
+
+  filter: drop-shadow(0 0 6px #000);
+
+  > span {
+    font-size: 14px;
+    font-weight: bold;
+  }
+
+  ::before {
+    content: '';
+    display: flex;
+
+    position: absolute;
+    left: -10px;
+    top: 17px;
+
+    width: 20px;
+    height: 20px;
+
+    transform: rotate(45deg);
+
+    background: #fff;
   }
 `;
