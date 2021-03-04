a1adc13b76c03e550ff9647db2f475b65be138fa
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-03-04 11:48:03 -0300

    fix: organizing routes

--- src/routes/Logged/index.tsx
+++ src/routes/Logged/index.tsx
@@ -1,17 +1,44 @@
 /** Packages */
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import BlurredBackground from '../../components/BlurredBackground';
 
 /** Pages */
+import HomePage from '../../pages/Home';
 import NotFound from '../../pages/NotFound';
-import CleanPage from '../../pages/CleanPage';
 
-const Routes: React.FC = () => (
-  <Switch>
-    <Route path="/" exact component={CleanPage} />
+const routes = [
+  {
+    id: 1,
+    name: 'Home',
+    path: '/',
+    exact: true,
+    component: <HomePage />,
+    backgroundUrl: 'https://i.imgur.com/d2ZYnJq.jpg',
+  },
+  {
+    id: 999,
+    name: 'NotFound',
+    path: '/',
+    exact: false,
+    component: <NotFound />,
+    backgroundUrl: null,
+  },
+];
 
-    <Route path="/" component={NotFound} />
-  </Switch>
-);
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      {routes.map(route => (
+        <Route path={route.path} exact={route.exact}>
+          {route.backgroundUrl && (
+            <BlurredBackground bgUrl={route.backgroundUrl} />
+          )}
+          {route.component}
+        </Route>
+      ))}
+    </Switch>
+  );
+};
 
 export default Routes;
